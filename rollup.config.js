dd337376514dfb2c4860b852c669be407d3cc0e0
Горбунов Василий <vasily.gorbunov@gmail.com>
2024-06-20 16:10:15 +0300

    init

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,6 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
-import { terser } from "rollup-plugin-terser";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import postcss from "rollup-plugin-postcss";
 import del from "rollup-plugin-delete";
